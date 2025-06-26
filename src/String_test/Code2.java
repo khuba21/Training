068061873ca7127ba8e0769e6fb9041f8d21f529
khuba21 <khuba2112@gmail.com>
2025-06-26 17:39:53 +0530

    commit

---
+++ src/String_test/Code2.java
@@ -0,0 +1,18 @@
+package String_test;
+
+public class Code2 {
+
+
+    public static void main(String[] args) {
+
+        String s1 = "Hello";
+        String s2 = "hello";
+        String s3 = "Hello";
+
+        System.out.println("equals():" +s1.equals(s3));
+        System.out.println("equalsIgnoreCase():"+s1.equalsIgnoreCase(s2));
+
+
+    }
+
+    }
