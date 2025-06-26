9725ef86d0be8453f24dac3ccc8ddac6b95abbbf
khuba21 <khuba2112@gmail.com>
2025-06-26 17:29:12 +0530

    ok

---
+++ src/String_test/Code1.java
@@ -0,0 +1,18 @@
+package String_test;
+
+public class Code1 {
+
+    public static void main(String[] args){
+
+        String s = "Hello World";
+
+        System.out.println("Length:" +s.length());
+
+        System.out.println("First char:" +s.charAt(0));
+
+        System.out.println("Substring: "+s.substring(6,11));
+
+
+
+    }
+}
